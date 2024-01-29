6bc9c9822cd20143cf4e98991b584cf2c3c4b528
notYellowMango <busimingi@gmail.com>
2024-01-29 17:31:48 +0900

    assets: 아이콘 임포트

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -3,10 +3,12 @@ import RightFill from "./rightFill.svg";
 import Search from "./search.svg";
 import NaviArrow from "./naviArrow.svg";
 import Delete from "./delete.svg";
+import EyeOpen from "./eye.svg";
 export const iconPaths = {
   Loading,
   RightFill,
   Search,
   NaviArrow,
   Delete,
+  EyeOpen,
 };
