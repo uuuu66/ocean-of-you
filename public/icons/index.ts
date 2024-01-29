77e6420cfbab3aef5f3217ca247ebc0c5d976d13
notYellowMango <busimingi@gmail.com>
2024-01-29 16:11:18 +0900

    assets: svg 임포트

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,4 +1,12 @@
 import Loading from "./loading.svg";
+import RightFill from "./rightFill.svg";
+import Search from "./search.svg";
+import NaviArrow from "./naviArrow.svg";
+import Delete from "./delete.svg";
 export const iconPaths = {
   Loading,
+  RightFill,
+  Search,
+  NaviArrow,
+  Delete,
 };
