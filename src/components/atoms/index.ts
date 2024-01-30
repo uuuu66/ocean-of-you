289adf34c6dab112ef96e24d220afbccce2aee23
notYellowMango <busimingi@gmail.com>
2024-01-30 18:06:29 +0900

    feat: atoms 파일들 묶음

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,12 @@
 import Button from "@/components/atoms/Button";
-
+import IconButton from "@/components/atoms/Button/IconButton";
+import Input from "@/components/atoms/Input";
+import IconInput from "@/components/atoms/Input/IconInput";
+import TextArea from "@/components/atoms/TextArea";
 export const atoms = {
   Button,
+  IconButton,
+  Input,
+  TextArea,
+  IconInput,
 };
