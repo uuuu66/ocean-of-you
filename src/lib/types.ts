6354de70a79fd8c04c3ec707086e1d7733b51756
notYellowMango <busimingi@gmail.com>
2024-01-30 18:06:13 +0900

    feat: startswith 타입 만듬

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,2 +1,3 @@
 export type Sizes = "xs" | "sm" | "md" | "lg" | "xl";
 export type ComponentTypes = "primary" | "secondary" | "green" | "red" | "gray";
+export type StartsWith<T extends string> = `${T}${string}`;
