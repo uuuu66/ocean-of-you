a4f5f41057c0dcccf252695956f008d90fe54eb4
minkiLee <64532732+uuuu66@users.noreply.github.com>
2024-02-15 22:25:45 +0900

    feat: drawer 추가

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
 export type ComponentSize = "xs" | "sm" | "md" | "lg" | "xl";
 export type ComponentType = "primary" | "secondary" | "green" | "red" | "gray";
-export type ComponentDirections = "";
+export type ComponentDirection = "left" | "right" | "top" | "bottom";
 export type StartsWith<T extends string> = `${T}${string}`;
