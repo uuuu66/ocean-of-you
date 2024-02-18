70efede2d80e47e46ea4fd240df25a7cd3f86a55
minkiLee <64532732+uuuu66@users.noreply.github.com>
2024-02-18 22:43:24 +0900

    update: 엔터키누를 경우 P태그를 만듬

--- src/components/common/Editor/index.tsx
+++ src/components/common/Editor/index.tsx
@@ -2,21 +2,31 @@ import React, { useCallback, useEffect, useRef } from "react";
 
 export default function Editor() {
   const contentRef = useRef<HTMLDivElement>(null);
-
+  const handleClickButton = useCallback((e: React.MouseEvent) => {
+    const selection = window.getSelection();
+    const range = selection?.getRangeAt(0);
+    if (range && selection) {
+      const clonedContents = range.cloneContents();
+      range?.deleteContents();
+      const span = document.createElement("span");
+      span.appendChild(clonedContents);
+      console.log(span.textContent, span.innerHTML, span.childNodes);
+    }
+  }, []);
   const handleKeydown = useCallback((e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
       // 엔터 키를 눌렀을 때
       e.preventDefault(); // 기본 동작을 막음
-
       const selection = window.getSelection();
       const range = selection?.getRangeAt(0);
-      if (range && selection) {
-        range.deleteContents();
-        const textNode = document.createTextNode("\n");
-        range.deleteContents();
+      if (range && selection && contentRef.current) {
+        const textNode = document.createTextNode("\n ");
         range.insertNode(textNode);
-        range.setEndAfter(textNode);
-        range.setStartAfter(textNode);
+        const p = document.createElement("p");
+        contentRef.current?.appendChild(p);
+        p.appendChild(textNode);
+        range.setStartBefore(textNode);
+        range.setEndBefore(textNode);
         selection.removeAllRanges();
         selection.addRange(range);
       }
@@ -24,17 +34,14 @@ export default function Editor() {
   }, []);
   return (
     <section aria-label="edtior">
-      <button>button</button>
+      <button onClick={handleClickButton}>button</button>
       <div>
         <div
           onKeyDown={handleKeydown}
           contentEditable
           ref={contentRef}
           className="border border-solid border-gray4 h-40 w-40 p-4 whitespace-pre-line"
-        >
-          sssss<strong>hi</strong>
-          <em>hi</em>ss
-        </div>
+        ></div>
       </div>
     </section>
   );
