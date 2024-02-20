207651dfd107ff1df939fa9983b11e46f2887168
slogup-mango <96713390+notYellowMango@users.noreply.github.com>
2024-02-20 12:07:26 +0900

    fix: p태그붙이는 기능 삭제   span태그 삽입하는 기능 구현중

--- src/components/headless/Editor/index.tsx
+++ src/components/headless/Editor/index.tsx
@@ -2,46 +2,121 @@ import React, { useCallback, useEffect, useRef } from "react";
 
 export default function Editor() {
   const contentRef = useRef<HTMLDivElement>(null);
-  const handleClickButton = useCallback((e: React.MouseEvent) => {
+  const moveCursorToTargetNode = useCallback((targetNode: Node) => {
     const selection = window.getSelection();
-    const range = selection?.getRangeAt(0);
-    if (range && selection) {
-      const clonedContents = range.cloneContents();
-      range?.deleteContents();
-      const span = document.createElement("span");
-      span.appendChild(clonedContents);
-      console.log(span.textContent, span.innerHTML, span.childNodes);
+
+    const targetRange = document.createRange();
+    targetRange.selectNodeContents(targetNode);
+    if (selection) {
+      targetRange.collapse(false);
+      // 현재 선택을 제거하고 새로운 선택을 설정합니다.
+      selection.removeAllRanges();
+      selection.addRange(targetRange);
     }
+    return targetRange;
   }, []);
-  const handleKeydown = useCallback((e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      // 엔터 키를 눌렀을 때
-      e.preventDefault(); // 기본 동작을 막음
+  const insertSpanToNodesInRange = useCallback(
+    (node: Node | null, startOffset: number = 0, endOffset: number = 0) => {
+      if (!node?.parentElement) return null;
+      switch (node.parentElement?.tagName) {
+        case "DIV":
+          {
+            const range = new Range();
+            range.setStart(node, startOffset);
+            range.setEnd(node, endOffset);
+            const clonedContents = range.cloneContents();
+            range.deleteContents();
+            const span = document.createElement("span");
+            span.appendChild(clonedContents);
+            range.insertNode(span);
+          }
+          break;
+        case "SPAN":
+          {
+            const range = new Range();
+            range.setStart(node, startOffset);
+            range.setEnd(node, endOffset);
+            const precededContent = range.cloneContents();
+
+            if (node.parentNode) {
+              const targetRange = moveCursorToTargetNode(node.parentNode);
+              const span = document.createElement("span");
+              span.appendChild(precededContent);
+              targetRange.insertNode(span);
+              node.parentNode.removeChild(node);
+            }
+          }
+          break;
+      }
+    },
+    [moveCursorToTargetNode]
+  );
+
+  const handleClickButton = useCallback(
+    (e: React.MouseEvent) => {
       const selection = window.getSelection();
       const range = selection?.getRangeAt(0);
-      if (range && selection && contentRef.current) {
-        const textNode = document.createTextNode("\n ");
-        range.insertNode(textNode);
-        const p = document.createElement("p");
-        contentRef.current?.appendChild(p);
-        p.appendChild(textNode);
-        range.setStartBefore(textNode);
-        range.setEndBefore(textNode);
-        selection.removeAllRanges();
-        selection.addRange(range);
+      const clonedRange = range?.cloneRange();
+      if (range && selection && clonedRange) {
+        const { anchorNode, anchorOffset, focusNode, focusOffset } = selection;
+        if (focusNode && anchorNode) {
+          if (anchorNode.isEqualNode(focusNode)) {
+            insertSpanToNodesInRange(anchorNode, anchorOffset, focusOffset);
+          } else {
+            if (anchorNode?.compareDocumentPosition(focusNode) === 2) {
+              insertSpanToNodesInRange(anchorNode, 0, anchorOffset);
+              insertSpanToNodesInRange(
+                focusNode,
+                focusOffset,
+                focusNode.textContent?.length || 0
+              );
+            } else {
+              insertSpanToNodesInRange(
+                anchorNode,
+                anchorOffset,
+                anchorNode.textContent?.length
+              );
+              insertSpanToNodesInRange(focusNode, 0, focusOffset);
+            }
+          }
+        }
       }
-    }
-  }, []);
+    },
+    [insertSpanToNodesInRange]
+  );
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent) => {
+      if (contentRef.current) {
+        const { childNodes } = contentRef.current;
+        for (let i = 0; i < childNodes.length; i += 1) {
+          const div = document.createElement("div");
+          if (childNodes[i].nodeType === 3) {
+            div.appendChild(childNodes[i].cloneNode());
+            contentRef.current.replaceChild(div, childNodes[i]);
+            moveCursorToTargetNode(div);
+          } else {
+            div.remove();
+          }
+        }
+      }
+    },
+    [moveCursorToTargetNode]
+  );
+
   return (
     <section aria-label="edtior">
       <button onClick={handleClickButton}>button</button>
       <div>
         <div
-          onKeyDown={handleKeydown}
+          onKeyUp={handleKeyDown}
           contentEditable
           ref={contentRef}
+          id="editor"
           className="border border-solid border-gray4 h-40 w-40 p-4 whitespace-pre-line"
-        ></div>
+        >
+          <div>123456789</div>
+          <div>abcdefghhhj</div>
+        </div>
       </div>
     </section>
   );
