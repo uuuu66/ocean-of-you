10aa0879b76da47fdf77841a149c7946d65f79d4
uuuu66 <uuuu66@naver.com>
2024-03-07 08:37:41 +0900

    feat: 부모의 p태그를 찾는 함수 완성

--- src/components/headless/Editor/nodeHandlers.ts
+++ src/components/headless/Editor/nodeHandlers.ts
@@ -233,10 +233,20 @@ const findTextNode = (node: Node) => {
   while (target?.firstChild) {
     if (!target) return target;
     if (target.nodeType === 3) {
-      console.log(target);
       return target;
     }
     target = target.firstChild;
+  }
+  return target;
+};
+const findParentPElement = (node: Node) => {
+  let target: Node | null = node;
+  while (target?.parentElement) {
+    if (!target) return target;
+    if (target.nodeName === "P") {
+      return target;
+    }
+    target = target.parentElement;
   }
   return target;
 };
