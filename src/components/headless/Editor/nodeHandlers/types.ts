5d4abb578e62e74ec1e4a3bd134814b462382a2a
uuuu66 <uuuu66@naver.com>
2024-03-08 13:52:48 +0900

    feat: types.ts 파일 만듬

---
+++ src/components/headless/Editor/nodeHandlers/types.ts
@@ -0,0 +1,25 @@
+import { TagName } from "@/components/headless/Editor";
+import { CSSProperties } from "react";
+
+export interface InsertTagNextToNodesArgs {
+  styleKey?: keyof CSSStyleDeclaration;
+  styleValue?: string;
+  spanStyle?: CSSProperties;
+  node: Node | null;
+  startOffset?: number;
+  endOffset?: number;
+  content?: Node | null;
+  tagName?: TagName;
+}
+export interface FlattendNode {
+  node: Node | null;
+  style: CSSStyleDeclaration | null;
+  isParentNode: boolean | null;
+  text: string;
+  nodeIndex?: number[];
+}
+export interface RecomposedNodes {
+  firstNode: Node | null;
+  lastNode: Node | null;
+  originalNode: Node;
+}
