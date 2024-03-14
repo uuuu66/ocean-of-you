f2f0349856edec1a20c4ad6aee9c794f514d6cab
uuuu66 <uuuu66@naver.com>
2024-03-14 15:54:07 +0900

    refactor:파일 분리

--- src/components/headless/Editor/eventHandlers.ts
+++ src/components/headless/Editor/eventHandlers.ts
@@ -1,15 +1,8 @@
-import { classNames } from "@/components/headless/Editor/configs";
-import { insertTagAtOffsets } from "@/components/headless/Editor/nodeHandlers/common";
+import { pasteNodesToSelection } from "@/components/headless/Editor/nodeHandlers/pasteNodes";
 import { recomposeNode } from "@/components/headless/Editor/nodeHandlers/recomposeNode";
-import {
-  searchParentNodeForNodeName,
-  searchTextNode,
-} from "@/components/headless/Editor/nodeHandlers/searchNodes";
-import { FlattendNode } from "@/components/headless/Editor/nodeHandlers/types";
-import { ZCOOL_KuaiLe } from "next/font/google";
-import { start } from "repl";
+import { searchTextNodeAtOffset } from "@/components/headless/Editor/nodeHandlers/searchNodes";
 
-export const handleEditorKeyDown = (
+const handleEditorKeyDown = (
   e: React.KeyboardEvent,
   targetElement?: HTMLElement | null
 ) => {
@@ -24,8 +17,10 @@ export const handleEditorKeyDown = (
     }
 
     switch (e.code) {
+      case "KeyX":
       case "MetaLeft":
       case "MetaRight":
+      case "KeyD":
       case "KeyV":
       case "KeyC":
         if (e.ctrlKey || e.metaKey) break;
@@ -87,10 +82,22 @@ export const handleEditorKeyDown = (
           span.appendChild(br);
           targetElement.appendChild(p);
         }
+        if (
+          targetElement.firstChild?.firstChild &&
+          targetElement.firstChild.firstChild.nodeName === "BR"
+        ) {
+          const span = document.createElement("span");
+          const br = document.createElement("br");
+          span.appendChild(br);
+          targetElement.firstChild.replaceChild(
+            span,
+            targetElement.firstChild.firstChild
+          );
+        }
     }
   }
 };
-export const handleEditorFocus = (
+const handleEditorFocus = (
   e: React.FocusEvent,
   targetElement?: HTMLElement | null
 ) => {
@@ -105,179 +112,51 @@ export const handleEditorFocus = (
     }
   }
 };
-export const handleEditorAfterPaste = (
+const handleEditorAfterPaste = (
   e: React.ClipboardEvent<HTMLElement>,
   targetElement?: HTMLElement | null
 ) => {
-  if (e.clipboardData.getData("text/plain") && targetElement) {
+  if (targetElement) {
     e.preventDefault();
+
     const div = document.createElement("div");
     div.innerHTML = e.clipboardData.getData("text/html");
     const recomposedNode = recomposeNode(div);
     pasteNodesToSelection(recomposedNode, targetElement);
   }
 };
-
-const pasteNodesToSelection = (
-  resultArray: FlattendNode[],
+const handleEditorCut = (
+  e: React.ClipboardEvent<HTMLElement>,
   targetElement?: HTMLElement | null
 ) => {
-  if (!resultArray) {
-    console.error("need resultArray");
+  if (!targetElement) {
     return;
   }
+  e.preventDefault();
   const selection = window.getSelection();
-  if (!selection) {
-    console.error("need selection");
-    return;
-  }
-  const range = selection.getRangeAt(0);
-  if (!range) {
-    console.error("need range");
-    return;
-  }
+  const range = selection?.getRangeAt(0);
+  if (!range) return;
+  const data = range?.extractContents();
+  const div = document.createElement("div");
+  if (data) div.appendChild(data);
 
-  const { anchorNode, anchorOffset, focusNode, focusOffset } = selection;
-  if (!anchorNode || !focusNode) {
-    console.error("need AnchorNode or FocusNode");
-    return;
-  }
-  let startNode = anchorNode;
-  let startOffset = anchorOffset || 0;
-  let endOffset = focusOffset || 0;
-  let endNode = focusNode;
-  if (!range.collapsed) {
-    //anchorNode,focusNode간의 위치 선후 관계를 비교한 후 분기
-    //2 뒤에서 앞으로
-    if (anchorNode?.compareDocumentPosition(focusNode) === 2) {
-      startNode = focusNode;
-      startOffset = focusOffset;
-      endOffset = anchorOffset;
-      endNode = anchorNode;
-    } else if (anchorNode?.compareDocumentPosition(focusNode) === 0) {
-      startNode = focusNode;
-      startOffset = Math.min(anchorOffset, focusOffset);
-      endOffset = Math.max(anchorOffset, focusOffset);
-      endNode = focusNode;
-    }
-    range.deleteContents();
-    endOffset = startOffset;
-  }
+  e.clipboardData.setData("text/html", div.innerHTML);
 
-  let parentP = searchParentNodeForNodeName(startNode, "P");
-  switch (!!parentP) {
-    case true:
-      if (resultArray[0]?.childNodes) {
-        const fragment = document.createDocumentFragment();
-        for (let i = 0; i < resultArray[0]?.childNodes?.length; i += 1) {
-          if (i === resultArray[0]?.childNodes?.length - 1) {
-            resultArray[0].childNodes[i].className = classNames.lastNode;
-          }
-          fragment.appendChild(resultArray[0].childNodes[i]);
-        }
-        insertTagAtOffsets({
-          node: startNode,
-          startOffset,
-          endOffset,
-          content: fragment,
-        });
-      }
+  range?.collapse(true);
+  const cursorFlag = document.createElement("span");
+  cursorFlag.className = "hi";
+  range.insertNode(cursorFlag);
+  selection?.removeAllRanges();
+  range.setStartBefore(cursorFlag);
+  range.setEndBefore(cursorFlag);
 
-      break;
-    case false:
-      {
-        const p = document.createElement("p");
-        if (resultArray[0]?.childNodes) {
-          const fragment = document.createDocumentFragment();
-          for (let i = 0; i < resultArray[0]?.childNodes?.length; i += 1) {
-            if (i === resultArray[0]?.childNodes?.length - 1)
-              resultArray[0].childNodes[i].className = classNames.lastNode;
-            fragment.appendChild(resultArray[0].childNodes[i]);
-          }
-          p.appendChild(fragment);
-
-          if (
-            targetElement &&
-            targetElement.childNodes?.length === 1 &&
-            targetElement.firstChild
-          )
-            targetElement.removeChild(targetElement.firstChild);
-          range.insertNode(p);
-        }
-      }
-      break;
-  }
-
-  const lastNode = moveCursorToLastNode(selection);
-  let clonedNode = null;
-  if (!lastNode) {
-    console.error("need lastNode");
-    return;
-  }
-
-  parentP = searchParentNodeForNodeName(lastNode, "P");
-  const newRange = new Range();
-  newRange.setStartAfter(lastNode);
-  if (parentP?.lastChild) newRange.setEndAfter(parentP?.lastChild);
-  clonedNode = newRange.cloneContents();
-  if (clonedNode.textContent) newRange.deleteContents();
-
-  if (resultArray.length > 1) {
-    if (parentP) {
-      let nextPastePoint = new Range();
-      nextPastePoint.setEndAfter(parentP);
-      nextPastePoint.setStartAfter(parentP);
-      for (let i = 1; i < resultArray.length; i += 1) {
-        const { node } = resultArray[i];
-        if (!node) break;
-        node.firstChild?.parentElement?.setAttribute(
-          "class",
-          classNames.lastNode
-        );
-        nextPastePoint.insertNode(node);
-        const lastAddedNode = moveCursorToLastNode(
-          selection,
-          !clonedNode?.textContent
-        );
-        nextPastePoint = selection.getRangeAt(0);
-
-        if (
-          i === resultArray.length - 1 &&
-          clonedNode?.textContent &&
-          lastAddedNode?.lastChild
-        ) {
-          newRange.setStartAfter(lastAddedNode.lastChild);
-          newRange.setEndAfter(lastAddedNode.lastChild);
-          newRange.insertNode(clonedNode);
-        }
-      }
-    }
-    moveCursorToLastNode(selection, !clonedNode?.textContent);
-  } else {
-    newRange.setEndAfter(lastNode);
-    newRange.setStartAfter(lastNode);
-    newRange.insertNode(clonedNode);
-  }
+  // cursorFlag.remove();
+  selection?.addRange(range);
 };
-const moveCursorToLastNode = (
-  selection: Selection,
-  deleteClassName: boolean = true
-) => {
-  const lastNode = document.getElementsByClassName(classNames.lastNode)[0];
-  selection.removeAllRanges();
-  const newRange = new Range();
-  if (!lastNode?.lastChild) {
-    console.error("no lastChild", lastNode);
-    return null;
-  }
-  const targetNode = searchTextNode(lastNode?.lastChild);
-  if (!targetNode) return null;
-  if (targetNode?.textContent) {
-    newRange.setStart(targetNode, 0);
-    newRange.setEnd(targetNode, targetNode.textContent?.length);
-    newRange.collapse(false);
-    selection.addRange(newRange);
-    if (deleteClassName) lastNode.removeAttribute("class");
-  }
-  return lastNode;
+
+export {
+  handleEditorFocus,
+  handleEditorAfterPaste,
+  handleEditorKeyDown,
+  handleEditorCut,
 };
---
+++ src/components/headless/Editor/nodeHandlers/pasteNodes.ts
@@ -0,0 +1,208 @@
+import { classNames } from "@/components/headless/Editor/configs";
+import { insertTagAtOffsets } from "@/components/headless/Editor/nodeHandlers/common";
+import {
+  searchParentNodeForNodeName,
+  searchTextNodeAtOffset,
+  searchTextNode,
+} from "@/components/headless/Editor/nodeHandlers/searchNodes";
+import { FlattendNode } from "@/components/headless/Editor/nodeHandlers/types";
+
+const pasteNodesToSelection = (
+  resultArray: FlattendNode[],
+  targetElement?: HTMLElement | null
+) => {
+  if (!resultArray) {
+    console.error("need resultArray");
+    return;
+  }
+  const selection = window.getSelection();
+  if (!selection) {
+    console.error("need selection");
+    return;
+  }
+  const range = selection.getRangeAt(0);
+  if (!range) {
+    console.error("need range");
+    return;
+  }
+  const { anchorNode, anchorOffset, focusNode, focusOffset } = selection;
+  if (!anchorNode || !focusNode) {
+    console.error("need AnchorNode or FocusNode");
+    return;
+  }
+
+  let startNode = anchorNode;
+  let startOffset = anchorOffset || 0;
+  let endOffset = focusOffset || 0;
+  if (!range.collapsed) {
+    //anchorNode,focusNode간의 위치 선후 관계를 비교한 후 분기
+    //2 뒤에서 앞으로
+
+    if (anchorNode?.compareDocumentPosition(focusNode) === 2) {
+      console.log("hi");
+      startNode = focusNode;
+      startOffset = focusOffset;
+      endOffset = anchorOffset;
+    } else if (anchorNode?.compareDocumentPosition(focusNode) === 0) {
+      startNode = focusNode;
+      startOffset = Math.min(anchorOffset, focusOffset);
+      endOffset = Math.max(anchorOffset, focusOffset);
+    }
+    range.deleteContents();
+
+    endOffset = startOffset;
+  }
+  const parentP = searchParentNodeForNodeName(startNode, "P");
+  if (startNode.nodeName === "P") {
+    const { node, offset } = getAppropriateNodeAndOffset(
+      startNode,
+      Math.max(anchorOffset, focusOffset)
+    );
+    startNode = node;
+    startOffset = offset;
+    endOffset = offset;
+  }
+  const firstChildNode = resultArray[0];
+
+  switch (!!parentP) {
+    case true:
+      if (firstChildNode?.childNodes) {
+        const fragment = document.createDocumentFragment();
+        for (let i = 0; i < firstChildNode.childNodes?.length; i += 1) {
+          if (i === firstChildNode.childNodes?.length - 1) {
+            firstChildNode.childNodes[i].className = classNames.lastNode;
+          }
+          fragment.appendChild(firstChildNode.childNodes[i]);
+        }
+        insertTagAtOffsets({
+          node: startNode,
+          startOffset,
+          endOffset,
+          content: fragment,
+        });
+      }
+      break;
+    case false:
+      {
+        const p = document.createElement("p");
+        if (resultArray[0]?.childNodes) {
+          const fragment = document.createDocumentFragment();
+          for (let i = 0; i < resultArray[0]?.childNodes?.length; i += 1) {
+            if (i === resultArray[0]?.childNodes?.length - 1)
+              resultArray[0].childNodes[i].className = classNames.lastNode;
+            fragment.appendChild(resultArray[0].childNodes[i]);
+          }
+          p.appendChild(fragment);
+
+          if (
+            targetElement &&
+            targetElement.childNodes?.length === 1 &&
+            targetElement.firstChild
+          )
+            targetElement.removeChild(targetElement.firstChild);
+          range.insertNode(p);
+        }
+      }
+      break;
+  }
+  const lastNode = moveCursorToClassName(selection, classNames.lastNode);
+
+  insertRemainingNodes(lastNode, selection, resultArray);
+};
+const getAppropriateNodeAndOffset = (node: Node, offset: number) => {
+  switch (node.nodeName) {
+    case "P":
+      const {
+        childNode,
+
+        offset: resultOffset,
+      } = searchTextNodeAtOffset(node, offset);
+
+      return {
+        node: childNode,
+        offset: (resultOffset || 0) + 1,
+      };
+    default:
+      return { node, offset };
+  }
+};
+const insertRemainingNodes = (
+  lastNode: Node | null,
+  selection: Selection,
+  resultArray: FlattendNode[]
+) => {
+  if (resultArray.length <= 1) return;
+  if (!lastNode) {
+    console.error("need lastNode");
+    return;
+  }
+  const lastTextNode = searchTextNode(lastNode);
+  if (!lastTextNode) {
+    console.error("need lastTextNode");
+    return;
+  }
+  const range = new Range();
+  const firstLineParentP = searchParentNodeForNodeName(lastNode, "P");
+  if (!firstLineParentP) {
+    console.error("need firstLineParentP");
+    return;
+  }
+  //뒤에 남아있는 노드들 추출하기
+  range.setStartAfter(lastNode);
+  if (firstLineParentP?.lastChild)
+    range.setEndAfter(firstLineParentP?.lastChild);
+  const remainingNodes = range.cloneContents();
+  range.deleteContents();
+  //p태그들 집어넣기
+  let nextPastePoint = new Range();
+  nextPastePoint.setEndAfter(firstLineParentP);
+  nextPastePoint.setStartAfter(firstLineParentP);
+  for (let i = 1; i < resultArray.length; i += 1) {
+    const { node } = resultArray[i];
+    if (!node) break;
+    node.firstChild?.parentElement?.setAttribute("class", classNames.lastNode);
+    nextPastePoint.insertNode(node);
+    const lastAddedNode = moveCursorToClassName(selection, classNames.lastNode);
+
+    const parentP = searchParentNodeForNodeName(node, "P");
+    if (!parentP) break;
+    nextPastePoint.setStartAfter(parentP);
+    nextPastePoint.setEndAfter(parentP);
+    parentP.lastChild?.parentElement?.setAttribute(
+      "class",
+      classNames.lastNode
+    );
+    moveCursorToClassName(selection, classNames.lastNode);
+    //마지막 p에 추출한 노드들 집어넣기
+    if (i === resultArray.length - 1 && lastAddedNode?.lastChild) {
+      range.setStartAfter(lastAddedNode.lastChild);
+      range.setEndAfter(lastAddedNode.lastChild);
+      range.insertNode(remainingNodes);
+    }
+  }
+};
+const moveCursorToClassName = (selection: Selection, className: string) => {
+  const targetNode = document.getElementsByClassName(className)[0];
+  selection.removeAllRanges();
+  const newRange = new Range();
+  if (!targetNode) {
+    console.error("no targetNode");
+    return null;
+  }
+  if (!targetNode?.lastChild) {
+    console.error("no lastChild", targetNode);
+    return null;
+  }
+  const textNode = searchTextNode(targetNode?.lastChild);
+  if (!textNode) return null;
+  if (textNode?.textContent) {
+    newRange.setStart(textNode, 0);
+    newRange.setEnd(textNode, textNode.textContent?.length);
+    newRange.collapse(false);
+
+    selection.addRange(newRange);
+    targetNode.removeAttribute("class");
+  }
+  return targetNode;
+};
+export { pasteNodesToSelection };
